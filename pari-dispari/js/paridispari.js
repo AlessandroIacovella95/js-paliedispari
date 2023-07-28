3f701a125267660b73008783efb0de2730159aad
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-07-28 10:36:24 +0200

    js: fix name var

--- pari-dispari/js/paridispari.js
+++ pari-dispari/js/paridispari.js
@@ -1,35 +1,40 @@
 //* Chiedo all'Utente di scegliere tra pari e dispari
 let userChoice = prompt('Pari o dispari?');
-
+    
 //* Ciclo While per far accettare solo i due valori necessari
 while (userChoice !== 'pari' && userChoice !== 'dispari') {
-    userChoice = prompt('Valori accettati: pari o dispari')
+    alert('Dati non validi');
+    userChoice = prompt('Pari o dispari?')
 }
-
+    
 console.log("Scelta dell'Utente: " + userChoice );
-
+    
 //* Chiedo all'Utente un numero compreso tra 1 e 5
-let userNumber = parseInt(prompt('inserisci un numero da 1 a 5'))
-
+let userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'))
+    
 //* Ciclo While per far accettare solo i numeri all'interno dell'intervallo
 while (isNaN(userNumber) || userNumber > 5 || userNumber < 1) {
-    userNumber = prompt('valore non valido, inserisci un numero da 1 a 5')
+    alert ('Dati non validi');
+    userNumber = prompt('Inserisci un numero da 1 a 5')
 }
-
+    
 console.log("Numero dell'Utente: " + userNumber );
+
+let minimo = 1;
+let massimo = 5;
 
 //* Tramite funzione genero randomicamente un numero compreso tra 1 a 5
 function randomNumber(min, max) {
-    const radomPcNumber = (Math.floor(Math.random () * ((max + 1) - min)) + min);
+    const radomPcNumber = Math.floor(Math.random () * ((max + 1) - min)) + min;
     return radomPcNumber
 }
 
-const pcNumber = randomNumber(1, 5)
+const pcNumber = randomNumber(minimo, massimo);
 
 console.log('Numero random del PC: ' + pcNumber);
 
 //* Sommo il numero dell'Utente con il numero random
-let sum = pcNumber + userNumber
+const sum = pcNumber + userNumber
 
 console.log("La somma è: " + sum);
 
@@ -46,10 +51,10 @@ function isEven(number) {
     return result  
 }
 
-const even = isEven(sum)
+const evenSum = isEven(sum)
 
 //* Dichiaro il vincitore tramite condizione "if"
-if ((userChoice === 'pari' && even) || (userChoice === 'dispari' && !even)) {
+if ((userChoice === 'pari' && evenSum) || (userChoice === 'dispari' && !evenSum)) {
     console.log('Utente hai vinto');
 } else {
     console.log('Pc hai vinto');
